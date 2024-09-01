05e4d987dd2078d7162de86f93d0a378adb111ca
Leraner <legend.tony@yandex.ru>
2024-09-01 11:50:09 +0300

    Check username of commits

--- main.py
+++ main.py
@@ -7,4 +7,6 @@ if __name__ == "__main__":
     os.system(
         "python3 -m grpc_tools.protoc --python_out=. --grpc_python_out=. --pyi_out=. --proto_path=. ./user_protos/*.proto"
     )
+
+
     asyncio.run(run_server())
